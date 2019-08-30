f861e15f9e243f26a6ecac115bfa406be3b001ea
5up4rhot <666smaxim@gmail.com>
2019-08-30 15:47:07 +0300

    int filter on DEBUG env var

--- web/lpr_project/lpr_project/settings.py
+++ web/lpr_project/lpr_project/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
+DEBUG = int(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'libertarian-party.herokuapp.com']
 
